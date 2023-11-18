78b81a83b9973a09d445b9bbcf7e751ea4bc0572
Jared Patterson <jpwesley34@gmail.com>
2023-11-17 17:00:57 -0800

    personalizes DKT's quest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // const newQuests = document.getElementbyId("new-task-description")
+  
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    editQuests(e.target.tasks.value)
+    form.reset()
+  })
 });
+
+function editQuests(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', removeQuest)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function removeQuest(e){
+  e.target.parentNode.remove()
+}
+
+//extra tasks https://learning.flatironschool.com/courses/7100/assignments/260399?module_item_id=619034
